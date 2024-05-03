9c6b0b7ebcc922d5380cbbe62019dbeaafa73b25
huydat-isme <datprono28@gmail.com>
2024-05-03 21:06:19 +0700

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html","./style.css"],
+  content: ["./index.html","./style.css","./products.html"],
   theme: {
     extend: {},
   },
